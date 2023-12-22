b369e0efa8d03aa838b13b69edc67a06013ba7bf
sri hari <sriharionahigh@gmail.com>
2023-12-22 10:05:28 +0530

    ui header

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -3,13 +3,14 @@ import Link from "next/link";
 export default function Navbar() {
   return (
     <div className="max-w-3xl mx-auto px-4 sm:px-6 lg:px-8">
-      <div className="flex justify-between h-16">
-        <div className="flex justify-between items-center w-full">
+      <div className="flex justify-between h-16 items-center">
+        <div className="flex items-center">
           <Link href="/">
-            <h1 className="text-2xl font-medium">
-              Bringer <span className="text-[#027FFA]">Blog</span>
-            </h1>
+            <img src="./bringer.svg" alt="Bringer logo" className="mr-2" />
           </Link>
+          <h1 className="text-2xl font-medium">
+            <span className="text-[#000000] font-bold">Bringer</span> <span className="text-[#080808]">Blog</span>
+          </h1>
         </div>
       </div>
     </div>
