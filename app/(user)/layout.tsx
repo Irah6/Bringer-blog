85ba8cbe05b2fea84400776a01077735677aede7
sri hari <sriharionahigh@gmail.com>
2023-12-24 10:44:11 +0530

    removed speed

--- app/(user)/layout.tsx
+++ app/(user)/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from 'next'
 import { Analytics } from '@vercel/analytics/react';
-import { SpeedInsights } from '@vercel/speed-insights/next';
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
 import { Inter } from 'next/font/google'
@@ -30,7 +29,6 @@ export default function RootLayout({
         <Image className="m-auto object-contain" loading="lazy" src={frame3} alt="cover-image"  />
         <main>{children}</main>
         <Analytics />
-        <SpeedInsights />
         <Footer/>
       </body>
     </html>
