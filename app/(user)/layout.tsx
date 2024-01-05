0592b12facf2ad245cc3566cbee65dd2c40e29cf
sri hari <sriharionahigh@gmail.com>
2024-01-05 15:28:42 +0530

    favicon

--- app/(user)/layout.tsx
+++ app/(user)/layout.tsx
@@ -6,6 +6,7 @@ import { Inter } from 'next/font/google'
 import Image from 'next/image';
 import frame3 from '../../public/frame_3.png'
 import '.././styles/globals.css'
+import Head from 'next/head';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -37,6 +38,12 @@ export default function RootLayout({
 
   return (
     <html lang="en">
+      <Head>
+      <link rel="icon" href="/favicon.ico" />
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+      </Head>
       <body className={` ${inter.className} bg-white text-black  h-full selection:bg-gray-50`}>
         <Navbar/>
         <Image className="m-auto object-contain"  src={frame3} alt="cover-image"  />
